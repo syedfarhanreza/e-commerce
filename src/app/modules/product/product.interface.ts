e96ac8658e3f18e170809cc3f401c526fef045b4
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-05-21 11:26:44 +0600

    product interface created

--- src/app/modules/product/product.interface.ts
+++ src/app/modules/product/product.interface.ts
@@ -1,1 +1,21 @@
+export type Variants = {
+  type: string;
+  value: string;
+};
 
+export type Inventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type Product = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: Variants[];
+  inventory: Inventory;
+};
+
+export default Product;
